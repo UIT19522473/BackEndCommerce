23fc18edc32da05adf51ea005f5bbdba56d90a38
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-07-05 10:59:49 +0700

    CRUD product

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require("./user");
+const productRouter = require("./product");
 const { notFound, errHandler } = require("../middleware/errHandler");
 
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
+  app.use("/api/product", productRouter);
 
   //routes handler when error
   app.use(notFound);
---
+++ routes/product.js
@@ -0,0 +1,18 @@
+const ctrls = require("../controllers/product");
+const router = require("express").Router();
+const { verifyAccessToken, isAdmin } = require("../middleware/verifyToken");
+
+//create new product
+router.post("/", [verifyAccessToken, isAdmin], ctrls.createProduct);
+
+//get all product
+router.get("/", ctrls.getProducts);
+
+//update product
+router.put("/:pid", [verifyAccessToken, isAdmin], ctrls.updateProduct);
+//update product
+router.delete("/:pid", [verifyAccessToken, isAdmin], ctrls.deleteProduct);
+//get one product by id
+router.get("/:pid", ctrls.getProduct);
+
+module.exports = router;
